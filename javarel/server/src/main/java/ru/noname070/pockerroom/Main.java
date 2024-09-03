a68383dcbd83015ea665c2dd7ed093f6c721695c
NN <61068649+noname070@users.noreply.github.com>
2024-09-04 01:51:10 +0300

    add logger to psvm

--- javarel/server/src/main/java/ru/noname070/pockerroom/Main.java
+++ javarel/server/src/main/java/ru/noname070/pockerroom/Main.java
@@ -1,12 +1,15 @@
 package ru.noname070.pockerroom;
 
 import java.util.Scanner;
+import java.util.logging.Level;
 
 import org.glassfish.tyrus.server.Server;
 
+import lombok.extern.java.Log;
 import ru.noname070.pockerroom.db.DBHandler;
 import ru.noname070.pockerroom.server.PockerServer;
 
+@Log
 public class Main {
     public static void main(String[] args) {
         DBHandler.getInstance(); // чекаем коннект к бд
@@ -19,17 +22,19 @@ public class Main {
 
             try (Scanner scanner = new Scanner(System.in);) {
                 while (true) {
-                    if (scanner.hasNextLine())
+                    if (scanner.hasNextLine()) {
                         if (scanner.nextLine().equals("q")) break;
+                    }
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.log(Level.OFF, "exception with server", e);
         } finally {
             try {
                 server.stop();
             } catch (Exception e) {
-                e.printStackTrace();
+                log.log(Level.OFF, "exception when stopping the server, bb+250", e);
+                System.exit(1);
             }
         }
     }
